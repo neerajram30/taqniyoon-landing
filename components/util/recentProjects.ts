81463a3f86e86841641a0835f27d8fa481a9e991
neeraj <neerajramachandran30@gmail.com>
2025-11-09 12:48:40 +0530

    fix

--- components/util/recentProjects.ts
+++ components/util/recentProjects.ts
@@ -40,7 +40,7 @@ export const recentProjects = [
         title:"Sharjah Municipality",
         subtitle:"Irrigation Control Panel",
         description:"Irrigation Control Panel Systems and Power Distribution Solutions",
-        usage:"🏟️ Infrastructure",
+        usage:"🏟️ Infrastructures",
         detail1:"135 KW & 95 KW",
         detail2:"Irrigation Panels",
         detail3:"Advanced",
